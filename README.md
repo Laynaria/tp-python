245d219ff74564ffedc583b391e5c35fddb0b617
laynaria <pro.laynaria@outlook.fr>
2025-12-19 13:42:11 +0100

    Exo2 : Part 1 done

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 ## Exercice 1 : launch titanic.py
 
+## Exercice 2 : Use Venv with source or other command
+
+### For me it was : `source /home/laynaria-wsl/tp/tp-python/.venv/bin/activate`
+
 # Notes for myself
 
 ### Code > Download Zip from Github
--- citations.py
+++ citations.py
@@ -1,8 +1,22 @@
+from quote import quote
+from pprint import pprint
+
 # 1. Concevoir un inventaire des titres de livres classés par fréquence d'apparition dans le résultat en ordre
 # décroissant de Edgar Allan Poe.
+search1 = "Edgar Allan Poe"
+result1 = quote(search1)
+
+def sortFunc(e):
+    return e.get("book")
+
+result1.sort(key=sortFunc, reverse=True)
+
+pprint(result1)
 
 # 2. Dresser un inventaire des mots classés par ordre décroissant de présence dans les citations de Edgar
 # Allan Poe. Ne pas afficher les mots présents moins de 5 fois.
+
+
 
 # 3. À partir de la liste des auteurs, générer 30 citations pour chacun de ces auteurs et générer un set des
 # mots en commun à tous ces auteurs. (10 citations si le pc est lent)
