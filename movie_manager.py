9b8b3eb4da24602b9b4def69675d8430c190f193
laynaria <pro.laynaria@outlook.fr>
2025-12-19 19:38:14 +0100

    Exo 3: Part 3B done

--- movie_manager.py
+++ movie_manager.py
@@ -50,17 +50,22 @@ class MovieManager:
             else:
                 print(f"No movie with this title!")
 
-# not working yet
     def get_movies_sorted_by_date(self):
         get_list = self.movie_list 
 
         get_list.sort(key=lambda x: datetime.strptime(x.released_date, '%d/%m/%Y'))
         return get_list
     
-# update_movie_by_title : Modifier un ou plusieurs champs d'un film trouvé par titre
+    def update_movie_by_title(self, title, new_title, new_released_date, new_description):
+        new_movie = Movie(new_title, new_released_date, new_description)
+
+        for movie in self.movie_list:
+            if movie.title.lower() == title.lower():
+                self.movie_list[self.movie_list.index(movie)] = new_movie
+        else:
+            print(f"No movie with this title!")
 
 
-    # def update_movie_by_title(self, title):
     def delete_movie_by_title(self, title):
             for movie in self.movie_list:
                 if movie.title.lower() == title.lower():
@@ -69,15 +74,18 @@ class MovieManager:
             else:
                 print(f"No movie with this title!")
 
-new_manager = MovieManager()
-new_manager.load_from_json()
+# new_manager = MovieManager()
+# new_manager.load_from_json()
 
-print("All movies")
-print(new_manager.get_all_movies())
+# print("All movies")
+# print(new_manager.get_all_movies())
 
 # test_movie = Movie("bla", "14/06/2020", "bla bla bla bla bla")
 # new_manager.create_movie(test_movie)
-new_manager.save_to_json()
+
+# new_manager.update_movie_by_title("bla", "blabla", "14/06/2020", "bla bla bla bla bla x2")
+
+# new_manager.save_to_json()
 
 # print("Get movie bla")
 # new_manager.get_movie_by_title("bla")
@@ -85,6 +93,6 @@ new_manager.save_to_json()
 # new_manager.delete_movie_by_title("bla")
 # print(new_manager.get_all_movies())
 
-print("Film list in order of released date")
-for movie in new_manager.get_movies_sorted_by_date():
-    print(movie, "\n")
+# print("Film list in order of released date")
+# for movie in new_manager.get_movies_sorted_by_date():
+#     print(movie, "\n")
