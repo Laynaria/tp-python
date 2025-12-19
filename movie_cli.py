813cf96b835c96b4990fb909d27a2844ad290258
laynaria <pro.laynaria@outlook.fr>
2025-12-19 20:15:40 +0100

    Exo 3: Part4: Cli functionnal, but functionnality 2 and 4 aren't answering all cases yet

--- movie_cli.py
+++ movie_cli.py
@@ -26,6 +26,34 @@ def movie_cli(movie_manager_to_use : MovieManager):
             print("\nAll Movies:")
             movie_manager_to_use.get_all_movies()
 
+# Proposer un sous-menu :
+# Afficher tous les films (triés par date)
+# Rechercher un film par titre
+# Retour au menu principal
+        
+        case "3":
+            movie_to_update = input("Enter the title of the Movie you want to update: ")
+            new_title = input("Enter New Title")
+            new_released_date = input("Enter New Released Date in dd/mm/yy format only: ")
+            new_description = input("Enter New Description: ")
+
+#  Afficher un sous-menu pour choisir quoi modifier (titre / date / description)
+
+
+            movie_manager_to_use.update_movie_by_title(movie_to_update, new_title, new_released_date, new_description)
+            movie_manager_to_use.save_to_json()
+
+        case "4":
+           title = input("Enter the title of the Movie you want to delete: ")
+           validation = input("Are you sure? (Y/N) ")
+
+           if (validation == "Y"):
+               movie_manager_to_use.delete_movie_by_title(title)
+               movie_manager_to_use.save_to_json()
+               print("\nAll Remaining Movies:")
+               movie_manager_to_use.get_all_movies()
+
+
         case "5":
             print("Goodbye")
             exit(0)
