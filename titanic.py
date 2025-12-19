cdd5b72f983f7303c1f6ae996c7d6985f6274671
laynaria <pro.laynaria@outlook.fr>
2025-12-19 12:05:39 +0100

    Exercice 1 - Question 2 done

--- titanic.py
+++ titanic.py
@@ -1,7 +1,12 @@
 import csv
 
 all_ages: list[float] = []
-age_average: float = 0
+
+first_class = []
+second_class = []
+third_class = []
+
+boats_that_saved = []
 
 with open("titanic_survival.csv", "r") as csv_file:
     csv_file_reader = csv.DictReader(csv_file)
@@ -15,9 +20,25 @@ with open("titanic_survival.csv", "r") as csv_file:
         age = line["age"]
         boat = line["boat"]
 
+        # part 1
         if age :
             all_ages.append(float(age))
+
+        # part 2
+        if pclass == "1":
+            first_class.append(int(survived))
+        elif pclass == "2":
+            second_class.append(int(survived))
+        elif pclass == "3":
+            third_class.append(int(survived))
+        
+        # part 3
+        if survived == "1" and boat:
+            boats_that_saved.append(boat)
     
+
+# Part 1 Logic
+age_average: float = 0
 
 for age in all_ages :
     age_average += age
@@ -25,3 +46,37 @@ for age in all_ages :
 age_average = age_average / len(all_ages)
 
 print(f"Average Age is : {age_average}") # Average Age is : 29.8811345124283
+
+# Part 2 Logic
+# First Class
+number_saved_first_class = 0
+
+for is_alive in first_class:
+    if is_alive:
+        number_saved_first_class += 1
+
+percent_saved_first_class = number_saved_first_class / len(first_class) * 100
+print(f"First class: {percent_saved_first_class} % survivor") # First class: 61.91950464396285 % survivor
+
+# Second class
+number_saved_second_class = 0
+
+for is_alive in second_class:
+    if is_alive:
+        number_saved_second_class += 1
+
+percent_saved_second_class = number_saved_second_class / len(second_class) * 100
+print(f"Second class: {percent_saved_second_class} % survivor") # Second class: 42.96028880866426 % survivor
+
+# Third Class
+number_saved_third_class = 0
+
+for is_alive in third_class:
+    if is_alive:
+        number_saved_third_class += 1
+
+percent_saved_third_class = number_saved_third_class / len(third_class) * 100
+print(f"Third class: {percent_saved_third_class} % survivor") # Third class: 25.52891396332863 % survivor
+
+# Part 3 Logic
+# for boat in boats_that_saved:
